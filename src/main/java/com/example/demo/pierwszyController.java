42e8065de3278be4824b66379c266e0f8e611f49
Mateusz Szybowski <matszyb@gmail.com>
2026-06-11 00:41:44 +0200

    Zad 1

--- src/main/java/com/example/demo/pierwszyController.java
+++ src/main/java/com/example/demo/pierwszyController.java
@@ -1,18 +1,21 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class pierwszyController {
 
     @RequestMapping("/")
     public String index() {
-        return "Nasz pierwszy projekt i już działający serwer :-) \n M.Szybowski";
+        return "Nasz pierwszy projekt i już działający serwer :-)";
     }
-    @RequestMapping("/tajne")
-    public String metoda() {
-        return "Tajny adres :-)";
+
+    @GetMapping(value = "/{tekst1}/{liczba1}/{tekst2}/{liczba2}")
+    public String PrzykladZPath(@PathVariable String tekst1, @PathVariable Long liczba1,
+                                @PathVariable String tekst2, @PathVariable Long liczba2
+    ) throws Exception {
+        return "Z adresu pozyskałem następujące dane: tekst1="+tekst1+", liczba1:"+liczba1+", " +
+                "tekst2="+tekst2+", liczba2 to:"+liczba2
+                ;
     }
 }
-
